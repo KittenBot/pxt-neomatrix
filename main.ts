b5d8874e8e470de9934b6e05c36d3c90e4d37d1b
Riven <4771215@qq.com>
2018-09-22 12:21:03 +0800

    more setup blocks

--- main.ts
+++ main.ts
@@ -11,21 +11,38 @@ namespace neomatrix {
 
     let rgbBuf: Buffer = pins.createBuffer(RGB_PIX * 3);
     let rgbPin: DigitalPin;
+    let matType: MatType;
+    let rgbBright: number = 30;
 
     // no hex literal support for microbit v1
     function hexstr2buf(a: string): void {
         for (let i = 0; i < a.length; i += 2) {
-            rgbBuf[i / 2] = (a.charCodeAt(i) % 32 + 9) % 25 * 16 + (a.charCodeAt(i + 1) % 32 + 9) % 25;
+            let rgb = (a.charCodeAt(i) % 32 + 9) % 25 * 16 + (a.charCodeAt(i + 1) % 32 + 9) % 25;
+            rgbBuf[i / 2] = (rgb * rgbBright) >> 8;
         }
         ws2812b.sendBuffer(rgbBuf, rgbPin);
     }
 
-    //% blockId=neomat_setpin block="Neo Pin %pin"
-    export function SetRGBPin(pin: DigitalPin): void {
+    export enum MatType {
+        //% block=32x8
+        m32x8 = 0,
+        //% block=16x16
+        m16x16 = 1
+    }
+
+    //% blockId=neomat_setpin block="Matrix Pin %pin %mat"
+    export function SetRGBPin(pin: DigitalPin, mat: MatType): void {
         rgbPin = pin;
+        matType = mat;
+    }
+
+    //% blockId=neomat_setbright block="Matrix Brightness %bright"
+    export function SetBright(bright: number): void {
+        rgbBright = bright & 0xff;
     }
 
     //% blockId=neomat_clear block="Clear"
+    //% blockGap=70
     export function NeoMatClear(): void {
         rgbBuf.fill(0);
         ws2812b.sendBuffer(rgbBuf, rgbPin);
